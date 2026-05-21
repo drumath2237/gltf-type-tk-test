652beff8697054ddf1cccd5a1061e161a9959132
drumath2237 <2237k.drumath@gmail.com>
2026-05-22 05:00:52 +0900

    :sparkles: succeeded load model

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 import gltf from "@todde.tv/gltf-type-toolkit/vite";
 
-export default defineConfig({
-  plugins: [gltf()],
+export default defineConfig(() => {
+  const baseUrl = "gltf-type-tk-test";
+  return {
+    plugins: [
+      gltf({
+        forceBasePath: baseUrl,
+        verbose: true,
+      }),
+    ],
+    base: `/${baseUrl}/`,
+  };
 });
